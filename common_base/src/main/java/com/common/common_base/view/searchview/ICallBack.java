a6ce4e7e57a71f568a92c5115fd421fc1343dd85
Jw0160 <wangb@mao-wo.com>
2017-12-29 15:38:20 +0800

    add searchview

---
+++ common_base/src/main/java/com/common/common_base/view/searchview/ICallBack.java
@@ -0,0 +1,10 @@
+package com.common.common_base.view.searchview;
+
+/**
+ * Created by Carson_Ho on 17/8/10.
+ */
+
+public interface ICallBack {
+    void SearchAciton(String string);
+
+}
---
+++ common_base/src/main/java/com/common/common_base/view/searchview/bCallBack.java
@@ -0,0 +1,9 @@
+package com.common.common_base.view.searchview;
+
+/**
+ * Created by Carson_Ho on 17/8/11.
+ */
+
+public interface bCallBack {
+    void BackAciton();
+}
